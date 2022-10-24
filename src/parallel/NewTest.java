4c83f79a63424e2256cb33afe409aa2015cb96f0
sumanth-5 <116346903+sumanth-5@users.noreply.github.com>
2022-10-24 20:09:18 +0530

    PARALLEL COMMIT

---
+++ src/parallel/NewTest.java
@@ -0,0 +1,33 @@
+package parallel;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class NewTest {
+	 @Test    
+	    public void f(){
+	            //First session of WebDriver
+		 System.setProperty("webdriver.chrome.driver","C:\\Users\\gudip\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	        
+	            WebDriver driver = new ChromeDriver();
+	            //Goto guru99 site
+	            driver.get("http://demo.guru99.com/V4/");
+}
+	 @Test    
+	    public void sandeal(){
+	            //First session of WebDriver
+	        System.setProperty("webdriver.chrome.driver","C:\\Users\\gudip\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	            WebDriver driver = new ChromeDriver();
+	            //Goto guru99 site
+	            driver.get("http://www.amazon.in");
+}	
+	 @Test    
+	    public void flipkart(){
+	            //First session of WebDriver
+	        System.setProperty("webdriver.chrome.driver","C:\\Users\\gudip\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	            WebDriver driver = new ChromeDriver();
+	            //Goto guru99 site
+	            driver.get("http://www.flipkart.in");
+}	 
+}
